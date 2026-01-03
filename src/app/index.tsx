b366dd9ecc96a9531371a221845fbff6c5fb3328
masaharu332 <91720663+masaharu332@users.noreply.github.com>
2026-01-03 17:22:42 +0900

    Header Styling (58)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,10 +4,11 @@ import { JSX } from 'react'
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
       
@@ -56,8 +57,28 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  header: {
+    backgroundColor: '#15681B',
+    height: 104,
+    justifyContent: 'flex-end',
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.7)',
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#fff',
+    fontWeight: 'bold'
   }
 
 })
