23695a44a1e824f821c44dfc67a86b9e8184c17a
masaharu332 <91720663+masaharu332@users.noreply.github.com>
2026-01-12 12:29:59 +0900

    Add Stack (79)

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
-import { Slot } from 'expo-router'
+import { Stack } from 'expo-router'
 import { JSX } from 'react' 
 
 const Layout = (): JSX.Element => {
-    return <Slot />
+    return <Stack />
 }
 export default Layout
